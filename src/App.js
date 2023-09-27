46f63321764bb1bd174d6bc24faa48fb0b08b1e8
iMuhammadAbdullahKhan <khalilzaijadoon178@gmail.com>
2023-09-27 14:33:53 +0500

    shop

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <div className="row" style={{border : "3px solid black", height : "30vh"}}>
+        <h1>BCS 5C - first row</h1>
+      </div>
+      <div className="row" style={{border : "3px solid red", height : "70vh"}}>
+        <div className="col-md-4" style={{border : "3px solid green", height : "100%"}}></div>
+        <div></div>
+      </div>
     </div>
   );
 }
