b2efddef3debfb7bc8747478307ad4cc50377a93
iMuhammadAbdullahKhan <khalilzaijadoon178@gmail.com>
2023-10-01 00:50:01 +0500

    eshop

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ function App() {
   return (
     <div className="container-fluid">
       <div className="row" style={{border : "3px solid black", height : "30vh"}}>
-        <h1>BCS 5C - first row</h1>
+        <h1>Area to Create Menus</h1>
+        
       </div>
       <div className="row" style={{border : "3px solid red", height : "70vh"}}>
-        <div className="col-md-4" style={{border : "3px solid green", height : "100%"}}></div>
-        <div></div>
+        <div className="col-md-4" style={{border : "3px solid green", height : "100%"}}><h2>Area to Create Form</h2></div>
+        
       </div>
     </div>
   );
